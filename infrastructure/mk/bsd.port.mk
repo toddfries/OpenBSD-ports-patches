208afb81fa758c06e11ffa4b4b6e3526bbff9301
Todd T. Fries <todd@fries.net>
2008-09-22 19:53:14 -0500

    sync

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.954 2008/08/20 10:33:50 simon Exp $
+#	$OpenBSD: bsd.port.mk,v 1.956 2008/09/19 13:00:30 espie Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
@@ -69,6 +69,7 @@ CLEANDEPENDS ?= No
 USE_SYSTRACE ?= No
 BULK ?= No
 RECURSIVE_FETCH_LIST ?= Yes
+WRKDIR_LINKNAME ?= 
 _FETCH_MAKEFILE ?= /dev/stdout
 WRKOBJDIR ?=
 FAKEOBJDIR ?=
@@ -740,7 +741,7 @@ _lt_libs += lib${_n:S/+/_/g:S/-/_/g:S/./_/g}_ltversion=${_v}
 
 # Create the generic variable substitution list, from subst vars
 SUBST_VARS += MACHINE_ARCH ARCH HOMEPAGE ^PREFIX ^SYSCONFDIR FLAVOR_EXT \
-	MAINTAINER ^BASE_PKGPATH ^LOCALBASE ^X11BASE
+	MAINTAINER ^BASE_PKGPATH ^LOCALBASE ^X11BASE ^TRUEPREFIX
 _tmpvars =
 
 _PKG_ADD_AUTO ?=
@@ -1986,6 +1987,9 @@ ${_WRKDIR_COOKIE}:
 	@rm -rf ${WRKDIR}
 	@mkdir -p ${WRKDIR} ${WRKDIR}/bin ${DEPDIR}
 #	@ln -s ${LOCALBASE}/bin/pkg-config ${WRKDIR}/bin
+.if !empty(WRKDIR_LINKNAME)
+	@ln -sf ${WRKDIR} ${.CURDIR}/${WRKDIR_LINKNAME}
+.endif
 	@${_MAKE_COOKIE} $@
 
 ${_EXTRACT_COOKIE}: ${_WRKDIR_COOKIE} ${_SYSTRACE_COOKIE}
