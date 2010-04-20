4d76fc5871ca3841441fbd1d11bb01116e5505c4
Todd T. Fries <todd@fries.net>
2010-04-20 16:10:27 -0500

    sync

--- infrastructure/mk/bsd.port.mk
+++ infrastructure/mk/bsd.port.mk
@@ -1,6 +1,6 @@
 #-*- mode: Makefile; tab-width: 4; -*-
 # ex:ts=4 sw=4 filetype=make:
-#	$OpenBSD: bsd.port.mk,v 1.991 2010/04/17 10:16:11 espie Exp $
+#	$OpenBSD: bsd.port.mk,v 1.993 2010/04/20 19:58:09 landry Exp $
 #	$FreeBSD: bsd.port.mk,v 1.264 1996/12/25 02:27:44 imp Exp $
 #	$NetBSD: bsd.port.mk,v 1.62 1998/04/09 12:47:02 hubertf Exp $
 #
