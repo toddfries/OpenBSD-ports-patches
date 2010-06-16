e866979b666ea81ee3bc7092613ae8adf4109063
Todd T. Fries <todd@fries.net>
2010-06-16 03:34:26 -0500

    sync

--- lang/lua/lua.port.mk
+++ lang/lua/lua.port.mk
@@ -1,6 +1,15 @@
-# $OpenBSD: lua.port.mk,v 1.1 2008/06/11 05:00:01 landry Exp $
+# $OpenBSD: lua.port.mk,v 1.4 2010/06/15 22:05:24 jasper Exp $
 
 CATEGORIES+=	lang/lua
+
+# Major.Minor of current lua version provided by lang/lua
+MODLUA_VERSION=	5.1
+
+# Where the lua libraries will be installed to
+MODLUA_LIBDIR=	${PREFIX}/lib/lua/${MODLUA_VERSION}/
+
+# Where the lua modules will be installed to
+MODLUA_DATADIR=	${PREFIX}/share/lua/${MODLUA_VERSION}/
 
 RUN_DEPENDS+=	::lang/lua
 
