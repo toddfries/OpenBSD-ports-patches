f20c25edfeea882e140089d80b630070dfc97ec4
Todd T. Fries <todd@fries.net>
2014-01-22 12:35:48 -0600

    sync

--- devel/quirks/files/Quirks.pm
+++ devel/quirks/files/Quirks.pm
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 # ex:ts=8 sw=4:
-# $OpenBSD: Quirks.pm,v 1.116 2014/01/07 15:06:38 sthen Exp $
+# $OpenBSD: Quirks.pm,v 1.119 2014/01/22 12:00:32 sthen Exp $
 #
 # Copyright (c) 2009 Marc Espie <espie@openbsd.org>
 #
@@ -330,6 +330,7 @@ my $stem_extensions = {
 	'wcb' =>'tklib',
 	'foomatic-filters' =>'cups-filters',
 	'faad-xmms' => 'xmms-faad',
+	'p5-MojoX-Renderer-TT' => 'p5-Mojolicious-Plugin-TtRenderer',
 };
 
 # reasons for obsolete packages
@@ -356,6 +357,8 @@ my $obsolete_reason = {
 	'metasploit' => 2,
 	'childsplay-plugins' => 3,
 	'ez-ipupdate' => 3,
+	'tesseract-dan-frak' => 3,
+	'apc-upsd' => 3,
 };
 
 # ->is_base_system($handle, $state):
@@ -404,7 +407,7 @@ sub filter_obsolete
 			    $msg[$reason]);
 		} else {
 			push(@$list, $pkgname);
-	    	}
+		}
 	}
 }
 
